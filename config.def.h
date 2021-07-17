9888a2fe3da14792d2dd64a8242ededcb750e1e5
Xi Xia <againxx@mail.ustc.edu.cn>
2021-07-17 10:11:31 +0800

    Apply actualfullscreen patch

--- config.def.h
+++ config.def.h
@@ -130,6 +130,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_m,            setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_semicolon,    setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_semicolon,    togglefloating, {0} },
+    { MODKEY|ShiftMask,             XK_f,            togglefullscr,  {0} },
     { MODKEY,                       XK_s,            togglesticky,   {0} },
 	{ MODKEY,                       XK_0,            view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,            tag,            {.ui = ~0 } },
